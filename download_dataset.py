20123e1494a2e76ce215dae7de2b39d0dc47b5d2
jirslad <jirisladky970@gmail.com>
2023-02-16 23:59:46 +0100

    added main()

--- download_dataset.py
+++ download_dataset.py
@@ -1,19 +1,23 @@
 from torchvision import datasets, transforms
 from pathlib import Path
 
-dataset_path = Path("dataset")
+def main():
+    datasets_path = Path("datasets")
 
-train_dataset = datasets.DTD(root=dataset_path,
-                             split="train",
-                             transform=transforms.ToTensor(),
-                             download=True)
+    train_dataset = datasets.DTD(root=datasets_path,
+                                split="train",
+                                transform=transforms.ToTensor(),
+                                download=True)
 
-val_dataset = datasets.DTD(root=dataset_path,
-                           split="val",
-                           transform=transforms.ToTensor(),
-                           download=True)
-
-test_dataset = datasets.DTD(root=dataset_path,
-                            split="test",
+    val_dataset = datasets.DTD(root=datasets_path,
+                            split="val",
                             transform=transforms.ToTensor(),
                             download=True)
+
+    test_dataset = datasets.DTD(root=datasets_path,
+                                split="test",
+                                transform=transforms.ToTensor(),
+                                download=True)
+
+if __name__ == "__main__":
+    main()
