e1cb5dc89374617d5faa734e8833fb1d23494e09
jirslad <jirisladky970@gmail.com>
2023-02-21 18:54:26 +0100

    added print about dataloaders

--- train.py
+++ train.py
@@ -19,4 +19,11 @@ train_dataloader, val_dataloader, test_dataloader = datasets.create_dataloaders(
 
 class_names = train_dataloader.dataset.dataset.class_names
 
+print(f"Created DataLoaders with {len(train_dataloader)} training images," \
+    f"{len(val_dataloader)} validation images and {len(test_dataloader)} testing images.")
+print(f"Dataset contains {len(class_names)} classes.")
+
+
+
+
 aa = 1
