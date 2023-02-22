2a1c1a8f891891df8df078a5197a4931ce636f95
jirslad <jirisladky970@gmail.com>
2023-02-23 00:20:26 +0100

    first working version of training

--- train.py
+++ train.py
@@ -1,29 +1,69 @@
-import datasets
+import torch
+from torchvision import transforms
+from torchinfo import summary
+# from torchmetrics.functional.classification import multilabel_accuracy
 import os
 from pathlib import Path
 
+import datasets
+from models import TinyVGG
+from engine import train
+from utils import multilabel_accuracy
+
+SEED = 42
 NUM_WORKERS = os.cpu_count()
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print(f"Training on {device}.")
 
-### TEST FUNCTIONALITY
-dataset_path = Path("datasets/dtd/dtd")
-split_ratio = [0.6, 0.2, 0.2]
-BATCH_SIZE = 32
+def main():
 
-train_dataloader, val_dataloader, test_dataloader = datasets.create_dataloaders(
-    dataset_dir=dataset_path,
-    split_ratio=split_ratio,
-    transform=None,
-    batch_size=BATCH_SIZE,
-    num_workers=NUM_WORKERS
-)
+    ### TRANSFORM
+    transform = transforms.Compose([
+        transforms.Resize((64,64)),
+        transforms.ToTensor()
+    ])
 
-class_names = train_dataloader.dataset.dataset.class_names
+    ### DATASET ###
+    dataset_path = Path("datasets/dtd/dtd")
+    split_ratio = [0.6, 0.2, 0.2]
+    BATCH_SIZE = 32
 
-print(f"Created DataLoaders with {len(train_dataloader)} training images," \
-    f"{len(val_dataloader)} validation images and {len(test_dataloader)} testing images.")
-print(f"Dataset contains {len(class_names)} classes.")
+    train_dataloader, val_dataloader, test_dataloader = datasets.create_dataloaders(
+        dataset_dir=dataset_path,
+        split_ratio=split_ratio,
+        transform=transform,
+        batch_size=BATCH_SIZE,
+        num_workers=NUM_WORKERS,
+        seed=SEED
+    )
+
+    class_names = train_dataloader.dataset.dataset.class_names
+
+    print(f"Dataset contains {len(train_dataloader.dataset.dataset)} images of " \
+        f"{len(class_names)} classes., batch size is {BATCH_SIZE}. \n" \
+        f"DataLoaders have {len(train_dataloader)} training batches, {len(val_dataloader)} " \
+        f"validation batches and {len(test_dataloader)} testing batches."
+    )
+
+    ### MODEL ###
+    torch.manual_seed(SEED)
+    model_0 = TinyVGG(input_channels=3,
+                    hidden_channels=10,
+                    output_classes=len(class_names)).to(device)
+
+    summary(model_0, input_size=[1, 3, 64, 64])
+
+    ### TRAINING ###
+    EPOCHS = 5
+    accuracy_fn = multilabel_accuracy
+    loss_fn = torch.nn.BCEWithLogitsLoss()
+    optim = torch.optim.Adam(params=model_0.parameters(),
+                            lr=0.001)
+    train(model_0, train_dataloader, val_dataloader, loss_fn, optim,
+        EPOCHS, device, accuracy_fn)
 
 
-
+if __name__ == "__main__":
+    main()
 
 aa = 1
