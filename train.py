12a5ca36b9b9da5817d71f3904c2046b927dbf09
jirslad <jirisladky970@gmail.com>
2023-04-11 15:49:27 +0200

    changed LR

--- train.py
+++ train.py
@@ -183,8 +183,8 @@ def main(args):
     
     # learning rate scheduler
     scheduler = lr_scheduler.ChainedScheduler([
-        lr_scheduler.LinearLR(optim, start_factor=0.025, total_iters=max(2, args.epochs//6)),
-        lr_scheduler.ExponentialLR(optim, gamma=0.85)
+        lr_scheduler.LinearLR(optim, start_factor=0.025, total_iters=max(2, args.epochs//8)),
+        lr_scheduler.ExponentialLR(optim, gamma=0.9)
     ])
     # scheduler = None
 
